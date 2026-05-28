1b1ef2aa764568d08e0dca2341f1358c779b8797
umayer10-AI <mdumayer10@gmail.com>
2026-05-28 21:04:48 +0600

    add

--- src/components/SmoothScroll.jsx
+++ src/components/SmoothScroll.jsx
@@ -5,7 +5,7 @@ import Lenis from 'lenis';
 
 export default function SmoothScroll({ children }) {
   useEffect(() => {
-    // Initialize Lenis scroll engine
+
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Cubic-exponential easing
